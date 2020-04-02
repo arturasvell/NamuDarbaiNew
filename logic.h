71dc934377a35e563de384e5b5a663ef48e3e1fb
arturasvell <60686715+arturasvell@users.noreply.github.com>
2020-04-02 22:50:34 +0300

    Update logic.h

--- logic.h
+++ logic.h
@@ -11,10 +11,18 @@
 #include <algorithm>
 #include <deque>
 #include <sstream>
+#include <list>
+#include <vector>
 using namespace std;
 using std::cout;
 static bool medianShow = false;
 static const int threadNum = 4;
+enum Method
+{
+	VECTOR,
+	LIST,
+	DEQUE
+};
 class Student
 {
 public:
@@ -52,15 +60,33 @@ public:
 	}
 };
 Student* AddInfo(deque<Student> arr);
+
+Student* AddInfo1(vector<Student> arr);
+
+Student* AddInfo2(list<Student> arr);
+
 bool ReturnMedianQuestion();
 bool ReturnFileReadQuestion();
 void Program();
 void GenerationAlgorithm(int amountToGenerate, int counter);
 void GenerateTxtFiles(int amountOfFiles);
 string ConvertIntToString(int toConvert);
+
 void PrintElements(deque<Student> arr, deque<Student> best);
-void SortElements(deque<Student>& arr, deque<Student>& best);
+void SortElements(deque<Student>& arr, deque<Student>& best, deque<Student>& worst);
 void SortByName(deque<Student>& arr);
 void SortByResults(deque<Student>& arr);
 void bufer_nusk(std::string read_vardas, deque<Student>& arr);
+
+void PrintElements1(vector<Student> arr, vector<Student> best);
+void SortElements1(vector<Student>& arr, vector<Student>& best, vector<Student>& worst);
+void SortByName1(vector<Student>& arr);
+void SortByResults1(vector<Student>& arr);
+void bufer_nusk1(std::string read_vardas, vector<Student>& arr);
+
+void PrintElements2(list<Student> arr, list<Student> best);
+void SortElements2(list<Student>& arr, list<Student>& best, list<Student>& worst);
+void SortByName2(list<Student>& arr);
+void SortByResults2(list<Student>& arr);
+void bufer_nusk2(std::string read_vardas, list<Student>& arr);
 #endif // LOGIC_H
